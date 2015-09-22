4fcffbe2a165acf1e8c7260bad80206fca4ecd37
rayaraya <raya.orlova.98@mail.ru>
2015-09-23 01:50:54 +0300

    Fixed errors.

--- solutions/main.c
+++ solutions/main.c
@@ -1,72 +1,67 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 double solutions(double a, double b, double c)
 {
-    double x1, x2, x;
+    double x1, x2, x, d;
 
-
-    /*if (scanf("%lg %lg %lg", &a, &b, &c) != 3)
+    if (a == 0.0)
     {
-        printf("wrong input\n");
-        return 0;
-    }*/
-
-    if (a == 0)
-    {
-        if (b == 0)
+        if (b == 0.0)
         {
-            if (c == 0)
-                printf("R\n");
+            if (c == 0.0)
+                printf("Countless number of solutions.\n");
             else
-                printf("NO\n");
+                printf("Unfortunately, no solutions. :(\n");
         }
         else
         {
             x = -c / b;
-            if (x == -0)
-                x = 0;
-            printf("%lg", x);
+            if (x == -0.0)
+                x = 0.0;
+            printf("Equation has 1 solution: %lg\n", x);
         }
     }
     else
     {
-        if ((b * b - 4 * a * c) >= 0)
+        d = b * b - 4 * a * c;
+        if (d >= 0.0)
         {
-            x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-            x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-            if (x1 == -0)
-                x1 = 0;
-            if (x2 == -0)
-                x2 = 0;
+            x2 = (-b - sqrt(d)) / (2 * a);
+            x1 = (-b + sqrt(d)) / (2 * a);
+            if (x1 == -0.0)
+                x1 = 0.0;
+            if (x2 == -0.0)
+                x2 = 0.0;
             if (x1 == x2)
-                printf("%lg", x1);
+                printf("Equation has 1 solution: %lg\n", x1);
             else
             {
                 if (x1 > x2)
                 {
-                    printf("%lg %lg", x2, x1);
+                    printf("Equation has 2 solutions: %lg and %lg\n", x2, x1);
                 }
                 else
                 {
-                    printf("%lg %lg", x1, x2);
+                    printf("Equation has 2 solutions: %lg and %lg\n", x1, x2);
                 }
             }
         }
         else
-            printf("NO\n");
+            printf("Unfortunately, no solutions. :(\n");
     }
 
-    return 0;
 }
 
 int main ()
 {
     double a, b, c;
 
+    printf("Hello! Please, input coefficients of quadratic equation..\n");
+
     if (scanf("%lg %lg %lg", &a, &b, &c) != 3)
     {
-        printf("wrong input\n");
+        printf("Unfortunately, wrong input. :(\n");
         return 0;
     }
 
