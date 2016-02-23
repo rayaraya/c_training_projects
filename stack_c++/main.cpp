c33438e3c21d5927b232c6cf87877a3679a30287
rayaraya <raya.orlova.98@mail.ru>
2016-02-23 03:02:46 +0300

    +print stack, fixed a bug with variable "size_", +initialize static variable "number"

--- stack_c++/main.cpp
+++ stack_c++/main.cpp
@@ -3,65 +3,94 @@
 #include <cstdlib>
 #include <clocale>
 
-int i_len = 20;
-
-void command();
-
 class CStack
 {
 private:
     double *data_;
     int size_;
     int avl_length;
+    static int number;
 public:
-    int number = 0;
     CStack();
     ~CStack();
     void push(double val);
     double pop();
     int GetNum();
+    void print();
 };
 
-using namespace std;
+void command();
+
+int CStack::number = 0;
+int i_len = 20;
+
+double error_1 = 987654321;
 
 int main()
 {
     int com_1 = 0;
     int com_2 = 0;
 
-    cout << "If you want to create a stack enter 1." << endl;
-    cout << "If you want to close program enter 0." << endl;
+    std::cout << "If you want to close program enter 0." << std::endl;
+    std::cout << "If you want to create new stack 1." << std::endl;
 
-    cin >> com_1;
+    std::cin >> com_1;
 
-    if (!com_1) return 0;
-
-    while (com_1)
+    while (com_1 > 1 || com_1 < 0)
     {
-        if (com_1 == 1)
+        std::cout << "Incorrect command, please enter again." << std::endl;
+        std::cout << "If you want to close program enter 0." << std::endl;
+        std::cout << "If you want to create new stack 1." << std::endl;
+        std::cin >> com_1;
+    }
+
+    if (com_1)
+    {
+        CStack S1;
+
+        command();
+
+        std::cin >> com_2;
+
+        while(com_2)
         {
-            CStack S1;
-            command();
-            cin >> com_1;
-            if (com_1 == 2)
+            if (com_2 == 2)
             {
-                cout << "Please, enter a number of stack." << endl;
-                cin >> com_2;
-                S1.push(com_2);
+                std::cout << "Please enter number." << std::endl;
+                std::cin >> com_1;
+                S1.push(com_1);
                 command();
             }
+
             if (com_2 == 3)
             {
-                cout << "NUMBER:" << S1.pop() << endl;
+                com_1 = S1.pop();
+                if (com_1 == error_1)
+                {
+                    std::cout << "Result : ";
+                    std::cout << "Stack is empty."<< std::endl;
+                }
+                else
+                    std::cout << "Result : ";
+                    std::cout << com_1 << std::endl;
                 command();
             }
-            if (!com_1)
+
+            if (com_2 == 4)
             {
-                S1.~CStack();
-                return 0;
+                std::cout << "Result: ";
+                S1.print();
+                std::cout << "\n";
+                command();
             }
+
+            if (com_2 > 4 || com_2 < 2)
+            {
+                std::cout << "Incorrect command, please enter again." << std::endl;
+                command();
+            }
+            std::cin >> com_2;
         }
-        cin >> com_1;
     }
 
     return 0;
@@ -94,18 +123,42 @@ void CStack::push(double val)
 
 double CStack::pop()
 {
-    return data_[size_--];
+    double a;
+    if (size_ == 0)
+    return error_1;
+    a = data_[size_ - 1];
+    size_--;
+    return a;
 }
 
 void command()
 {
-    cout << "If you want to close program enter 0." << endl;
-    cout << "If you want to push a number enter 2 ." << endl;
-    cout << "If you want to pop and print number enter 3." << endl;
-    cout << "If you want to create new stack 1." << endl;
+    std::cout << "If you want to close program enter 0." << std::endl;
+    std::cout << "If you want to push a number enter 2 ." << std::endl;
+    std::cout << "If you want to pop and print number enter 3." << std::endl;
+    std::cout << "If you want to print all stack content enter 4." << std::endl;
 }
 
 int CStack::GetNum()
 {
     return number;
 }
+
+void CStack::print()
+{
+    if (size_== 0)
+    {
+        std::cout << "Stack is empty." << std::endl;
+        return;
+    }
+
+    int i;
+    double b;
+    for (i = size_; i > 0; i--)
+    {
+        b = data_[i - 1];
+
+        std::cout << b << std::cout << " ";
+    }
+}
+
